5ee2fbf610bc615d4a7a3d98cf55e77ea97f21b2
ian <ian.miell@container-solutions.com>
2025-07-22 13:19:39 +0100

    Move to evidence_id

--- init-timescaledb.sql
+++ init-timescaledb.sql
@@ -4,10 +4,10 @@ CREATE EXTENSION IF NOT EXISTS timescaledb;
 -- Create the evidence_ts table
 CREATE TABLE IF NOT EXISTS evidence_ts (
     ts TIMESTAMPTZ NOT NULL default now(),
-    evidence_uuid TEXT NOT NULL,
+    evidence_id uuid NOT NULL,
     evidence_status JSONB NOT NULL,
     labels JSONB NOT NULL,
-    PRIMARY KEY (ts, evidence_uuid)
+    PRIMARY KEY (ts, evidence_id)
 );
 
 -- Convert it to a hypertable
