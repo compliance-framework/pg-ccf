59b45e51b03010153010cf815434da1a57c1f13a
ian <ian.miell@container-solutions.com>
2025-07-23 11:21:26 +0100

    Changed name

--- init-timescaledb.sql
+++ init-timescaledb.sql
@@ -2,15 +2,15 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 -- Create the evidence_ts table
-CREATE TABLE IF NOT EXISTS evidence_ts (
-    ts TIMESTAMPTZ NOT NULL default now(),
-    evidence_uuid text NOT NULL,
+CREATE TABLE IF NOT EXISTS evidence_time_series (
+    evidence_time TIMESTAMPTZ NOT NULL default now(),
+    evidence_uuid TEXT NOT NULL,
     evidence_status JSONB NOT NULL,
     labels JSONB NOT NULL,
-    PRIMARY KEY (ts, evidence_uuid)
+    PRIMARY KEY (evidence_time, evidence_uuid)
 );
 
 -- Convert it to a hypertable
-SELECT create_hypertable('evidence_ts', 'ts', if_not_exists => TRUE);
+SELECT create_hypertable('evidence_time_series', 'evidence_time', if_not_exists => TRUE);
 
-CREATE INDEX ON evidence_ts USING GIN (labels);
+CREATE INDEX ON evidence_time_series USING GIN (labels);
